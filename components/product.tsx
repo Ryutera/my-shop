6c9eead938c3de0d98efe3e5824aa2a660dea84c
user@usernoMacBook-Air.local <Ryutera>
2025-05-30 10:34:55 +0100

    product component created

---
+++ components/product.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <div>
+    <div className="transition duration-300 transform hover:-translate-y-2 hover:scale-105 hover:shadow-2xl shadow-lg  overflow-hidden">
+            <img src="img/C59995E0-C29A-4350-ADF3-D9B375548E94.jpeg" alt="" className="h-[400px] "/>
+            </div>
+  
+  <div className="mt-3 justify-between flex"> 
+  <p>aaaaaaaaaaaaaa</p>
+  <p>100</p>
+  </div>
+</div>
+
+  )
+}
+
+export default Product
