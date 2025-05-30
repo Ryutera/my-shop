413b0fd474a61c9f37fbcf36bae5a291a71dead3
user@usernoMacBook-Air.local <Ryutera>
2025-05-30 23:43:01 +0100

    feat: create Product component and type definition

---
+++ lib/types.ts
@@ -0,0 +1,10 @@
+import { Asset, EntrySkeletonType } from "contentful"
+
+export type ProductFields = {
+    name: string
+    price: number
+    description: string
+    thumbnail: Asset
+    images: Asset[]
+  }
+ export type ProductFieldsSkeleton = EntrySkeletonType<ProductFields>
