214c8eb9f8c08188baf36021cb9d17f0e5b977ef
user@usernoMacBook-Air.local <Ryutera>
2025-05-30 09:57:20 +0100

    WIP: save local changes before merge

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,30 @@ export default async function Home() {
   return (
     <>
      
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        {/* <h2 className="font-medium text-xl mb-4">Next steps</h2> */}
-        {/* {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />} */}
-        
+      <main className="grid grid-cols-3 gap-6 px-4 ">
+        {/* <h2 className="font-medium text-xl mb-4">Next steps</h2>
+        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />} */}
+<div>
+  <img src="img/C59995E0-C29A-4350-ADF3-D9B375548E94.jpeg" alt="" className="h-[400px] "/>
+  <div className="mt-3 justify-between flex"> 
+  <p>aaaaaaaaaaaaaa</p>
+  <p>100</p>
+  </div>
+ 
+</div>
+<div>
+  <img src="img/C59995E0-C29A-4350-ADF3-D9B375548E94.jpeg" alt="" className="h-[400px]"/>
+</div>
+<div>
+  <img src="img/C59995E0-C29A-4350-ADF3-D9B375548E94.jpeg" alt="" className="h-[400px]"/>
+</div>
+<div>
+  <img src="img/C59995E0-C29A-4350-ADF3-D9B375548E94.jpeg" alt="" className="h-[400px]"/>
+</div>
+<div>
+  <img src="img/C59995E0-C29A-4350-ADF3-D9B375548E94.jpeg" alt="" className="h-[400px]"/>
+</div>
+
       </main>
     </>
   );
