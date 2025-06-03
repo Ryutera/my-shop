baf5ec443120e7d82d98dbad786d6d90ca2916a7
Ryutera <kuya9823@gmail.com>
2025-06-04 00:09:44 +0100

    feat button to payment page created

--- app/context/CartContext.tsx
+++ app/context/CartContext.tsx
@@ -1,10 +1,5 @@
-"use client"
-import { Product } from "@/lib/types";
+"use client";
 import React, { createContext, ReactNode, useContext, useState } from "react";
-
-
-
-
 type CartContextType = {
   items: string[];
   addItem: (id: string) => void;
@@ -13,47 +8,40 @@ type CartContextType = {
 };
 
 const CartContext = createContext<CartContextType | undefined>(undefined);
-
 export const Provider = ({ children }: { children: ReactNode }) => {
   const [items, setItems] = useState<string[]>([]);
 
   const addItem = (id: string) => {
     setItems((prev) => {
-      const exists = prev.some((i) => i === id)
-      if (exists) return prev 
-      return [...prev, id] 
-    })
-  }
+      const exists = prev.some((i) => i === id);
+      if (exists) return prev;
+      return [...prev, id];
+    });
+  };
 
-  const removeItem = (id:string)=>{
-    setItems((prev)=>{
+  const removeItem = (id: string) => {
+    if (window.confirm("Do you want to remove this item?")) {
+      setItems((prev) => {
         return prev.filter((i) => i !== id);
-    })
+      });
+    }
    
-  } 
+  };
 
-  const clearCart = () => setItems([])
+  const clearCart = () => setItems([]);
 
   return (
     <CartContext.Provider value={{ items, addItem, removeItem, clearCart }}>
       {children}
     </CartContext.Provider>
-  )
+  );
 };
-
-// カスタムフック
+// Custom hook
 export const useCart = () => {
-    const context = useContext(CartContext)
-    if (context === undefined) {
-      throw new Error("useCart must be used within a CartProvider")
-    }
-    return context
+  const context = useContext(CartContext);
+  if (context === undefined) {
+    throw new Error("useCart must be used within a CartProvider");
   }
-  
-
-
+  return context;
+};
 export default CartContext;
-
-
-
-  
