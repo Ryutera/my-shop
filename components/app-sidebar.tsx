2e3465aa0b4f81de6459416a532c94f39f5998af
Ryutera <kuya9823@gmail.com>
2025-06-22 12:33:59 +0100

    side bar creaed

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,67 @@
+import { Home, Heart, ShoppingCart, History } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "Favorite Items",
+    url: "/",
+    icon: Heart,
+  },
+  {
+    title: "Cart Items",
+    url: "/",
+    icon: ShoppingCart,
+  },
+  {
+    title: "Purchase History",
+    url: "#",
+    icon: History,
+  },
+//   {
+//     title: "Settings",
+//     url: "#",
+//     icon: Settings,
+//   },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
