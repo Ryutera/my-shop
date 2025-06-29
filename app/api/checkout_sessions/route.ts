a500bc88cd93760087846fd40aa5d1f1387692b7
Ryutera <kuya9823@gmail.com>
2025-06-29 18:28:49 +0100

    made adjustment on success page

--- app/api/checkout_sessions/route.ts
+++ app/api/checkout_sessions/route.ts
@@ -1,80 +1,69 @@
-import { NextResponse } from 'next/server'
-import { headers } from 'next/headers'
-import { stripe } from '@/lib/stripe';
-import { createClient } from '@/utils/supabase/server';
+import { NextResponse } from "next/server";
+import { headers } from "next/headers";
+import { stripe } from "@/lib/stripe";
+import { createClient } from "@/utils/supabase/server";
 
-
-
-
-export async function POST(req:Request) {
+export async function POST(req: Request) {
   try {
-    const headersList = await headers()
-    const origin = headersList.get('origin')
+    const headersList = await headers();
+    const origin = headersList.get("origin");
     const body = await req.json();
     const products = body.products || [];
 
-    console.log(products,"pu")
+    console.log(products, "pu");
 
-
-interface Product {
-  id: string
-  name: string
-  price: number
-  image: string
-}
+    interface Product {
+      id: string;
+      name: string;
+      price: number;
+      image: string;
+    }
 
     const supabase = await createClient();
     const {
       data: { user },
     } = await supabase.auth.getUser();
 
-    const userId = user?.id 
+    const userId = user?.id;
 
     // Create Checkout Sessions from body params.
     const session = await stripe.checkout.sessions.create({
-      
       line_items: products.map((product: any) => ({
         price_data: {
           currency: "gbp",
           product_data: {
-            name: product.name, 
-    metadata: {
-      product_id: product.id, 
-    },
-           
+            name: product.name,
           },
-          
-          unit_amount: product.price*100,
+
+          unit_amount: product.price * 100,
         },
         quantity: 1,
-        
       })),
-      billing_address_collection: 'auto',
+      billing_address_collection: "auto",
       shipping_address_collection: {
-        allowed_countries: ['JP', 'GB'],
+        allowed_countries: ["JP", "GB"],
       },
-     metadata: {
-  user_id: userId || null,
-  //メタデータは文字列のみ処理可能
-  products: JSON.stringify(
-    products.map((p: Product) => ({
-      id: p.id,
-      name: p.name,
-      price: p.price,
-      
-    })),
-  )
-},
-      mode: 'payment',
-      locale:'en',
+      metadata: {
+        user_id: userId || null,
+        //メタデータは文字列のみ処理可能
+        products: JSON.stringify(
+          products.map((p: Product) => ({
+            id: p.id,
+            name: p.name,
+            price: p.price,
+          }))
+        ),
+      },
+      mode: "payment",
+      locale: "en",
       success_url: `${origin}/success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${origin}/?canceled=true`,
     });
     return NextResponse.json({ url: session.url });
-  } catch (err:any) {
+  } catch (err: any) {
     return NextResponse.json(
       { error: err.message },
       { status: err.statusCode || 500 }
-    )
+    );
   }
 }
