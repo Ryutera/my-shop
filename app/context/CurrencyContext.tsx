f48a951a9ed2dae8f48f5824b1d70a901ce622c3
Ryutera <kuya9823@gmail.com>
2025-07-24 23:12:07 +0100

    currencycontext created

---
+++ app/context/CurrencyContext.tsx
@@ -0,0 +1,34 @@
+import { createContext, ReactNode, useState } from "react";
+
+type CurrencyContextType= {
+    currency: string
+    changeToEur: () => void
+    changeToGbp: ()=>void
+    changeToJpy:  ()=>void
+}
+
+const CurrencyContext = createContext<CurrencyContextType | undefined>(undefined)
+
+import React from 'react'
+
+export const CurrencyProvider  = ({children}:{children:ReactNode}) => {
+    const [currency, setCurrency] = useState("GBP")
+
+    const changeToJpy=()=>{
+        setCurrency("JPY")
+    }
+    const changeToEur=()=>{
+        setCurrency("EUR")
+    }
+    const changeToGbp=()=>{
+        setCurrency("GBP")
+    }
+
+  return (
+    <CurrencyContext.Provider value={{currency,changeToEur,changeToGbp,changeToJpy}}>
+       {children}
+    </CurrencyContext.Provider>
+  )
+}
+
+export default CurrencyContext
--- components/CurrencySelector.tsx
+++ components/CurrencySelector.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Select,
     SelectContent,
     SelectGroup,
     SelectItem,
     SelectTrigger,
     SelectValue, } from './ui/select'
+import CurrencyContext from '@/app/context/CurrencyContext'
 
 
 const CurrencySelector = () => {
+    const context = useContext(CurrencyContext);
+    if (!context) {
+      throw new Error("CurrencyContext is undefined. Wrap with <CurrencyProvider>");
+    }
+  
+    const { currency,changeToEur,changeToGbp,changeToJpy } = context;
+  
   return (
     <div className="ml-2">
       <Select>
@@ -16,8 +24,12 @@ const CurrencySelector = () => {
       </SelectTrigger>
       <SelectContent>
         <SelectGroup>
+            
           <SelectItem value="blueberry">GBP</SelectItem>
+          
           <SelectItem value="grapes">EUR</SelectItem>
+         
+          
           <SelectItem value="pineapple">JPY</SelectItem>
         </SelectGroup>
       </SelectContent>
