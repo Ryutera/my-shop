85f82daa5d9ffc3ebdd947d18155dabf78274a55
Ryutera <kuya9823@gmail.com>
2025-08-03 19:57:22 +0100

    Display selected image as the starting slide in dialog view

--- components/productImeges.tsx
+++ components/productImeges.tsx
@@ -1,112 +1,119 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
 import {
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
-  } from "@/components/ui/carousel";
-import { Asset } from 'contentful';
-import { Dialog ,
-  DialogContent,
-  DialogTrigger,
-  DialogTitle } from './ui/dialog';
-  type Props = {
-    thumbnail: Asset;
-    images: Asset[];
-  };
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Asset } from "contentful";
+import { Dialog, DialogContent, DialogTrigger, DialogTitle } from "./ui/dialog";
+import { ArrowLeft, ArrowRight } from "lucide-react";
+type Props = {
+  thumbnail: Asset;
+  images: Asset[];
+};
 
 const ProductImeges = ({ thumbnail, images }: Props) => {
+  const [dialogSlideIndex, setDialogSlideIndex] = useState(-1);
+
+  console.log(dialogSlideIndex, "えーぴーあい");
+  console.log(images[dialogSlideIndex], "いめじ");
+
   return (
     <>
-    <Dialog>
-    <DialogTitle className="sr-only">Product Image Gallery</DialogTitle>
-  <Carousel>
-  <DialogTrigger asChild>
-    <CarouselContent>
-      {/* Thumbnail image */}
-      <CarouselItem key="thumbnail">
-        <div className="flex justify-center">
-          <img
-            src={`https:${thumbnail.fields.file?.url}`}
-            alt=""
-            className="h-[400px] object-cover rounded-lg"
-          />
-        </div>
-      </CarouselItem>
+      <Dialog>
+        <DialogTitle className="sr-only">Product Image Gallery</DialogTitle>
+        <Carousel>
+          <CarouselContent>
+            {/* Thumbnail image */}
+            <CarouselItem key="thumbnail">
+              <div className="flex justify-center">
+                <DialogTrigger asChild>
+                  <img
+                    src={`https:${thumbnail.fields.file?.url}`}
+                    alt=""
+                    className="h-[400px] object-cover rounded-lg"
+                    onClick={() => setDialogSlideIndex(-1)}
+                  />
+                </DialogTrigger>
+              </div>
+            </CarouselItem>
 
-      {/* Images */}
-      {images.map((image, index) => (
-        <CarouselItem key={index}>
-          <div className="flex justify-center">
-            {image.fields.file?.url && (
-              <img
-                src={`https:${image.fields.file.url}`}
-                alt=""
-                className="h-[400px] object-cover rounded-lg"
-              />
-            )}
+            {/* Images */}
+            {images.map((image, index) => (
+              <CarouselItem key={index}>
+                <div className="flex justify-center">
+                  {image.fields.file?.url && (
+                    <DialogTrigger asChild>
+                      <img
+                        src={`https:${image.fields.file.url}`}
+                        alt=""
+                        className="h-[400px] object-cover rounded-lg"
+                        onClick={() => setDialogSlideIndex(index)}
+                      />
+                    </DialogTrigger>
+                  )}
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+
+          <div className="flex justify-center mt-4">
+            <CarouselPrevious className="static translate-y-0 mr-2" />
+
+            <CarouselNext className="static translate-y-0 ml-2" />
           </div>
-        </CarouselItem>
-      ))}
-    </CarouselContent>
-    </DialogTrigger>
-   
-    <div className="flex justify-center mt-4">
-        <CarouselPrevious className="static translate-y-0 mr-2" />
-        <CarouselNext className="static translate-y-0 ml-2" />
-      </div>
-  </Carousel>
+        </Carousel>
 
-  <DialogContent className="max-w-[70vw] max-h-[95vh] p-2">
-  <Carousel>
-    <CarouselContent>
-      {/* Thumbnail image */}
-      <CarouselItem key="thumbnail">
-      <div className="flex justify-center items-center min-h-[70vh]">
-          <img
-            src={`https:${thumbnail.fields.file?.url}`}
-            alt=""
-             className="max-h-[80vh] max-w-full object-contain rounded-lg"
-          />
+        <DialogContent className="max-w-[70vw] max-h-[95vh] p-2">
+          <div className="grid grid-cols-3 grid-row items-center">
+            <div className="flex items-center  justify-center">
             
-        </div>
-      </CarouselItem>
-
-      {/* Images */}
-      {images.map((image, index) => (
-        <CarouselItem key={index}>
-          <div className="flex justify-center">
-            {image.fields.file?.url && (
-              <img
-                src={`https:${image.fields.file.url}`}
-                alt=""
-                className="max-h-[80vh] max-w-full object-contain rounded-lg"
-              />
-            )}
-            
+              <button onClick={() => setDialogSlideIndex((prev) => prev===-1 ? -1: prev - 1)}>
+              {dialogSlideIndex===-1? <></>:   <ArrowLeft/>}
+              </button>
+            </div>
+            <Carousel>
+              <CarouselContent>
+                {dialogSlideIndex < 0 ? (
+                  <CarouselItem key="thumbnail">
+                    <div className="flex justify-center items-center min-h-[70vh]">
+                      <img
+                        src={`https:${thumbnail.fields.file?.url}`}
+                        alt=""
+                        className="max-h-[80vh] max-w-full object-contain rounded-lg"
+                      />
+                    </div>
+                  </CarouselItem>
+                ) : (
+                  <CarouselItem key={dialogSlideIndex}>
+                    <div className="flex justify-center items-center min-h-[70vh]">
+                      {images[dialogSlideIndex]?.fields.file?.url && (
+                        <img
+                          src={`https:${images[dialogSlideIndex].fields.file.url}`}
+                          alt=""
+                          className="max-h-[80vh] max-w-full object-contain rounded-lg"
+                        />
+                      )}
+                    </div>
+                  </CarouselItem>
+                )}
+              </CarouselContent>
+            </Carousel>
+            <div className="flex items-center  justify-center">
+              
+              <button onClick={() => setDialogSlideIndex((prev) =>prev===images.length-1? prev:prev+1)}>
+                {dialogSlideIndex===images.length-1? <></>:   <ArrowRight/>}
+              
+              </button>
+            </div>
           </div>
-          
-        </CarouselItem>
-        
-      ))}
-    
-    </CarouselContent>
-
-   
-    {/* <div className="flex justify-center mt-4"> */}
-    <CarouselPrevious className="left-10 bg-black/50 text-white hover:bg-black/70 border-none" />
-    <CarouselNext className="right-10 bg-black/50 text-white hover:bg-black/70 border-none" />
-      {/* </div> */}
-  </Carousel>
-     
-  </DialogContent>
-</Dialog>
-
-  
+        </DialogContent>
+      </Dialog>
     </>
-  
-  )
-}
+  );
+};
 
-export default ProductImeges
+export default ProductImeges;
