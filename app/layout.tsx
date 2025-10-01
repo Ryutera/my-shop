364ee6a36d476373209f8782ff9b4562d74614bb
user <user@macbookair.home>
2025-10-01 06:56:42 +0100

    feat: open search bar only when navigating via search icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,10 +64,10 @@ export default async function RootLayout({
                 enableSystem
                 disableTransitionOnChange
               >
-                <main className="min-h-screen flex flex-col items-center bg-gray-100">
+                <main className="min-h-screen flex flex-col items-center bg-white">
                   <div className="flex-1 w-full flex flex-col gap-20 items-center">
 
-                    <nav className="w-full flex justify-center border-b border-b-foreground/10 h-20 border-gray-400 bg-gray-200">
+                    <nav className="w-full flex justify-center border-b border-b-foreground/10 h-20 border-gray-400 bg-gray-100">
                     <SearchBar/>
                     <div className="w-full max-w-6xl flex justify-between items-center p-3 px-2 md:px-5 text-sm ">
                         <div className="flex gap-2 md:gap-5  items-center font-semibold">
