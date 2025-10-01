bcd584a1955ceee5337fd8e2b25365239398e981
user <user@macbookair.home>
2025-10-01 06:56:42 +0100

    feat: open search bar only when navigating via search icon

--- app/context/SearchContext.tsx
+++ app/context/SearchContext.tsx
@@ -15,22 +15,26 @@ export const SearchProvider = ({ children }: { children: React.ReactNode }) => {
     const pathname = usePathname()
     const router = useRouter()
      const prevPathnameRef = useRef<string>(pathname)
+     const shouldOpenSearchRef = useRef<boolean>(false)
 
-    const onClickSearch = () => {
-        //ホーム画面以外から検索を行っても検索結果が表示されないので検索アイコンを押すと自動で遷移するように設計
-        if (pathname !== "/") {
-            router.push("/")
-        } else {
-            setIsClicked((prev) => !prev)
-        }
 
+const onClickSearch = () => {
+    // ホーム画面以外から検索を行っても検索結果が表示されないので検索アイコンを押すと自動で遷移するように設計
+    if (pathname !== "/") {
+      shouldOpenSearchRef.current = true
+      router.push("/")
+    } else {
+      setIsClicked((prev) => !prev)
     }
+  }
 
-   useEffect(() => {
+  useEffect(() => {
     const isNavigatingToHome = prevPathnameRef.current !== "/" && pathname === "/"
 
-    if (isNavigatingToHome) {
+    //遷移先が"/" かつ 検索アイコンをクリックした　という条件でホーム遷移後検索バー表示
+    if (isNavigatingToHome && shouldOpenSearchRef.current) {
       setIsClicked(true)
+      shouldOpenSearchRef.current = false // フラグをリセット
     } else if (pathname !== "/") {
       // ホーム以外のページに遷移したら閉じる
       setIsClicked(false)
@@ -38,6 +42,7 @@ export const SearchProvider = ({ children }: { children: React.ReactNode }) => {
 
     prevPathnameRef.current = pathname
   }, [pathname])
+
 
     console.log(isClicked)
 
