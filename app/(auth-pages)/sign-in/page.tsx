dfda6267248da16f4c611facfb65e167ccd334ef
Ryutera <kuya9823@gmail.com>
2025-10-14 19:07:10 +0100

    .

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -13,7 +13,7 @@ export default async function Login(props: {
 
   
   return (
-    <form className="flex-1 flex flex-col min-w-64">
+    <form className="flex-1 flex flex-col min-w-64 ">
       <h1 className="text-2xl font-medium">Sign in</h1>
       <p className="text-sm text-foreground">
         Don't have an account?{" "}
